7c1f566ae3a6c1294ba7cf280f5163224b11e9b1
wangziye <154110628+solomon0817@users.noreply.github.com>
2026-04-29 13:04:34 +0800

    Delete multi_agent_code_optimizer directory

--- multi_agent_code_optimizer/src/agent_performance_optimizer.py
+++
@@ -1,8 +0,0 @@
-def optimize_performance(code):
-    # Example performance optimization: improve algorithm complexity
-    if "for i in range(n)" in code:
-        code = code.replace("for i in range(n)", "for i in range(1, n+1)")  # Optimize loop range
-
-    # Add more performance optimizations here
-
-    return code
